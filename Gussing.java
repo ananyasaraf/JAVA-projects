bda6a6209a1e495fd76b6f5de8a64e6f9a6f8b4c
ananyasaraf <90857982+ananyasaraf@users.noreply.github.com>
2024-07-22 20:30:42 +0530

    Add files via upload

---
+++ Gussing.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+public class Gussing {
+    public static void main(String[] args) {
+         Random rand=new Random();
+        int b;
+         int a = rand.nextInt(100);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to gussing game \n Enter a no");
+        for(int i=100;i>0;i--){
+            b=sc.nextInt();
+            if(a==b){
+                break;
+            }
+            else if(a>b){
+                System.out.println("select a grater number");
+            }
+            else if(a<b){
+                System.out.println("Select aa smaller no.");
+            }
+          
+
+        }
+        System.out.println("Cong!! you win");
+    }
+}
